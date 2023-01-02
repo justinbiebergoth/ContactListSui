75d98c3ee80c1ff63fd409cf28d90fc452b637e6
justinbiebergoth <hiirari666@gmail.com>
2023-01-02 18:29:52 +0300

    Initial Commit

---
+++ ContactListSui/ContactListSuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSuiApp.swift
+//  ContactListSui
+//
+//  Created by hiirari on 02.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
